aba92a01aa1945a160d1f4b2043a502ec21b759d
14_Galeh kya caka T. <galeh.tkjsmeksa25@gmail.com>
2026-08-10 10:57:41 +0700

    tugas 2_ganjil genap
    Updated the program to continuously check if numbers are odd or even until the user decides to exit.

--- Ganjil genap.py
+++ Ganjil genap.py
@@ -1,5 +1,12 @@
-x = int(input("masukan angka: "))
-if x % 2 == 0:
-    print("angka genap")
-else:
-    print("angka ganjil")
+"Menentukan Apakah Angka Ganjil atau Genap"
+
+while True:
+	x = int(input("Silahkan Masukkan Angka: "))
+	if x % 2 == 0:
+		print("Angka", x, "adalah Bilangan Genap")
+	else:
+		print("Angka", x, "adalah Bilangan Ganjil")
+
+	tombol = input("Tekan 'x' untuk keluar atau tekan tombol lain untuk melanjutkan:")
+	if tombol == 'x':
+		break
