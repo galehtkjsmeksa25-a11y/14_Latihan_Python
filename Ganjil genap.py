fbd1b41bf93ea410e707aae8939d48dcc0b6d34a
14_Galeh kya caka T. <galeh.tkjsmeksa25@gmail.com>
2026-08-29 06:59:04 +0700

    Add files via upload

--- Ganjil genap.py
+++ Ganjil genap.py
@@ -1,12 +1,5 @@
-"Menentukan Apakah Angka Ganjil atau Genap"
-
-while True:
-	x = int(input("Silahkan Masukkan Angka: "))
-	if x % 2 == 0:
-		print("Angka", x, "adalah Bilangan Genap")
-	else:
-		print("Angka", x, "adalah Bilangan Ganjil")
-
-	tombol = input("Tekan 'x' untuk keluar atau tekan tombol lain untuk melanjutkan:")
-	if tombol == 'x':
-		break
+x = int(input("masukan angka: "))
+if x % 2 == 0:
+    print("angka genap")
+else:
+    print("angka ganjil")
