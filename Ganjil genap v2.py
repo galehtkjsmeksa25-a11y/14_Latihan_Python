079bd3fcf444c3e64a201ecd9be954e7836210b3
14_Galeh kya caka T. <galeh.tkjsmeksa25@gmail.com>
2026-08-31 08:52:02 +0700

    Add files via upload

--- Ganjil genap v2.py
+++ Ganjil genap v2.py
@@ -1,8 +1,9 @@
 # Program Cek Bilangan Ganjil atau Genap
 
-bilangan = int(input("Masukkan sebuah bilangan: "))
+while True:
+    bilangan = int(input("Masukkan sebuah bilangan: "))
 
-if bilangan % 2 == 0:
-    print("Bilangan tersebut adalah GENAP")
-else:
-    print("Bilangan tersebut adalah GANJIL")
+    if bilangan % 2 == 0:
+        print("Bilangan tersebut adalah GENAP")
+    else:
+        print("Bilangan tersebut adalah GANJIL")
